ae285374795af5f63d584cf4c4f5ad64c443580d
Joseph Gu <aceralon@outlook.com>
2020-08-15 20:37:29 +0800

    添加砖块破坏

--- AutoPixel/Assets/Scripts/Logic/Core/Ground/Ground.cs
+++ AutoPixel/Assets/Scripts/Logic/Core/Ground/Ground.cs
@@ -1,9 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Logic.Core.Ground
 {
     public class Ground : MonoBehaviour
     {
+        public bool Life = true;
         
+        public GroundType Type;      
+
+        public int X, Y;        
+
+        public bool Damage()
+        {
+            if (Life)
+            {
+                Life = false;
+                return false;
+            }
+            else
+            {
+                gameObject.SetActive(false); 
+                return true;
+            }
+        }
+
+        public enum GroundType
+        {
+            Edge,
+            Inner
+        }
     }
 }
---
+++ AutoPixel/Assets/Scripts/Logic/Core/Ground/GroundDestory.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Logic.Core.Ground;
+using static Logic.Core.Ground.Ground;
+
+namespace Logic.Core.Ground
+{
+    public class GroundDestory : MonoBehaviour
+    {
+
+        public GameObject _Ground;
+        public float CheckTime;
+
+        void Start()
+        {
+            StartCoroutine("TimeDestory");
+        }
+
+        private IEnumerator TimeDestory()
+        {
+            while (true)
+            {
+                if (_Ground != null)
+                {
+                    int size = _Ground.transform.childCount;
+                    if (size > 0)
+                    {
+                        int rn;
+                        do
+                        {
+                            rn = Random.Range(0, size - 1);
+                        } while (_Ground.GetComponentsInChildren<Ground>()[rn].Type != GroundType.Edge);
+
+                        if (_Ground.GetComponentsInChildren<Ground>()[rn].Damage())
+                        {
+                            int X = _Ground.GetComponentsInChildren<Ground>()[rn].X,
+                            Y = _Ground.GetComponentsInChildren<Ground>()[rn].Y;
+                            for (int i = -1; i <= 1; i += 2)
+                            {
+                                for (int j = -1; j <= 1; j += 2)
+                                {
+                                    if (X + i >= 0 && X + i < 4 && Y + j >= 0 && Y + j < 5)
+                                    {
+                                        GameObject.Find($"G{X + i}{Y + j}").GetComponent<Ground>().Type
+                                            = GroundType.Inner;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                yield return new WaitForSeconds(CheckTime);
+            }
+        }
+    }
+}
+
+
